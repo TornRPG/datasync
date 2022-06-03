4ca95acc83043b301dc0a7b6f08f4bfd880c970d
NotJansel <janselfritz096@gmail.com>
2022-06-03 09:10:08 +0200

    beta1 publishing

--- src/main/kotlin/de/notjansel/datapacksync/Datapacksync.kt
+++ src/main/kotlin/de/notjansel/datapacksync/Datapacksync.kt
@@ -43,7 +43,7 @@ class Datapacksync : JavaPlugin() {
     }
 
     private fun remorselessnessAgainstFiles() {
-        for (file in File(serverpath + "/plugins").listFiles()) {
+        for (file in File(serverpath + "/plugins").listFiles()!!) {
             if (file.name.contains("datapacksync")) {
                 if (!file.name.contains(version)) {
                     file.delete()
@@ -54,7 +54,7 @@ class Datapacksync : JavaPlugin() {
 
     @Throws(IOException::class)
     private fun prepareRequisites() {
-        if (!File(serverpath + "/downloads/version.json").exists()) {
+        if (!File("$serverpath/downloads/version.json").exists()) {
             val file = File(serverpath + "downloads/version.json")
             val fetchsite = URL("https://raw.githubusercontent.com/TornRPG/datasync/master/version.json")
             FileUtils.copyURLToFile(fetchsite, file)
@@ -62,7 +62,7 @@ class Datapacksync : JavaPlugin() {
     }
 
     override fun onDisable() {
-        configfile.save(File(serverpath + "/plugins/Datapacksync/config.yml"))
+        configfile.save(File("$serverpath/plugins/Datapacksync/config.yml"))
     }
 
     companion object {
@@ -70,10 +70,10 @@ class Datapacksync : JavaPlugin() {
         var worlds: List<World>? = null
         var datapackManager: DatapackManager? = null
         var server: Server? = null
-        var versiontype: VersionTypes = VersionTypes.DEVELOPMENT;
+        var versiontype: VersionTypes = VersionTypes.BETA;
         lateinit var configfile: FileConfiguration
         lateinit var plugininstance: Plugin
-        const val version = "0.31.0-dev"
+        const val version = "0.31.0-beta1"
 
         @Throws(IOException::class)
         fun downloadFile(url: String?, path: String?) {
